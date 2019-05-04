4490e00b4ea0f8c2758c726cfdc7f366b8e91d6c
fayzi0552 <fayzi0552@gmail.com>
2019-05-04 19:47:44 +0500

    committed

--- resources/views/backend/settings/edit.blade.php
+++ resources/views/backend/settings/edit.blade.php
@@ -12,26 +12,26 @@
                         @method('PUT')
                         <div class="form-group">
                             {{ Form::label('facebook_link',__('Facebook Link')) }}
-                            {{ Form::text('facebook_link', old('facebook_link', $settings->firstWhere('slug','facebook_link')->value),['class'=> 'form-control']) }}
+                            {{ Form::text('facebook_link', old('facebook_link', optional($settings->firstWhere('slug','facebook_link'))->value),['class'=> 'form-control']) }}
                         </div>
                         <div class="form-group">
                             {{ Form::label('linkedin_link',__('Linkedin Link')) }}
-                            {{ Form::text('linkedin_link', old('linkedin_link', $settings->firstWhere('slug','linkedin_link')->value),['class'=> 'form-control']) }}
+                            {{ Form::text('linkedin_link', old('linkedin_link', optional($settings->firstWhere('slug','linkedin_link'))->value),['class'=> 'form-control']) }}
                         </div>
 
                         <div class="form-group">
                             {{ Form::label('twitter_link',__('Twitter Link')) }}
-                            {{ Form::text('twitter_link', old('twitter_link', $settings->firstWhere('slug','twitter_link')->value),['class'=> 'form-control']) }}
+                            {{ Form::text('twitter_link', old('twitter_link', optional($settings->firstWhere('slug','twitter_link'))->value),['class'=> 'form-control']) }}
                         </div>
 
                         <div class="form-group">
                             {{ Form::label('github_link',__('Github Link')) }}
-                            {{ Form::text('github_link', old('github_link', $settings->firstWhere('slug','github_link')->value),['class'=> 'form-control']) }}
+                            {{ Form::text('github_link', old('github_link', optional($settings->firstWhere('slug','github_link'))->value),['class'=> 'form-control']) }}
                         </div>
 
                         <div class="form-group">
                             {{ Form::label('stackoverfollow_link',__('Stackoverfollow Link')) }}
-                            {{ Form::text('stackoverfollow_link', old('stackoverfollow_link', $settings->firstWhere('slug','stackoverfollow_link')->value),['class'=> 'form-control']) }}
+                            {{ Form::text('stackoverfollow_link', old('stackoverfollow_link', optional($settings->firstWhere('slug','stackoverfollow_link'))->value),['class'=> 'form-control']) }}
                         </div>
 
                         <div class="form-group">
