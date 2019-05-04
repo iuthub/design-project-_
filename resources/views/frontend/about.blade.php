b322c8db590675531bb5f3f8eec16c64fd902a0d
fayzi0552 <fayzi0552@gmail.com>
2019-05-04 19:55:44 +0500

    committed

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -1,6 +1,72 @@
 @extends('layouts.frontend')
 
 @section('content')
+    <div class="row">
+        <div class="col-lg-10 offset-1">
+            <p>My name is <strong>Moh. Ibrahim Al Naz Rana</strong> from Khulna, Bangladesh. I am a CSE postgraduate and
+                I am also a full stack web developer. I love programming, travelling, reading books and learning new
+                things.</p>
 
+            <p>I defined myself as a hard worker, fast learner, early adapter, team guy, self motivated and passionate
+                web developer. I would like to take challenges and build beautiful, user-friendly, fully functional and
+                delivering the project within deadline.</p>
+
+            <h4>Skills</h4>
+            <p>PHP is my primary language for backend programming. I am doing my professional PHP development since
+                2013.
+                I am using the following PHP frameworks -</p>
+            <ul>
+                <li>Laravel (5.x)</li>
+                <li>CakePHP (2.x, 3.x)</li>
+                <li>Codeigniter (2.x, 3.x)</li>
+                <li>WordPress</li>
+            </ul>
+
+            <p>For frontend I am using the following -</p>
+            <ul>
+                <li>HTML5</li>
+                <li>CSS3</li>
+                <li>JavaScript</li>
+                <li>jQuery</li>
+                <li>Ajax</li>
+                <li>Bootstrap (3x, 4.x)</li>
+                <li>Vue JS (2.x)</li>
+                <li>React JS (Learning)</li>
+            </ul>
+
+            <p>The tool I use for developing -</p>
+            <ul>
+                <li><strong>Database:</strong> MYSQL, Redis, MongoDB</li>
+                <li><strong>Cache Storage:</strong> Redis, Memcached</li>
+                <li><strong>Message Broker:</strong> Redis, RabbitMQ</li>
+                <li><strong>Test:</strong> PHP Unit</li>
+                <li><strong>Linux OS:</strong> Ubuntu</li>
+                <li><strong>Server:</strong> Apache/Nginx</li>
+                <li>MYSQL Workbench</li>
+                <li>PHPStorm</li>
+                <li>Git</li>
+            </ul>
+
+            <p>I believe in <strong>code quality</strong> and follow the best practices. I follow <strong>PSRs coding
+                    standard</strong>, popular <strong>design patterns</strong> and <strong>agile software
+                    development</strong> process. I use <strong>Jetbrains IDEs</strong> for optimum productivity and
+                cleaner codes. I follow the for developing.</p>
+
+            <h4>Experiences</h4>
+            <p>I am a Co-Founder and Senior Software Engineer at <strong>CODEMEN</strong>. I have over 5 years
+                experience in software industry. Last 2 years, I lead two teams that helped me to grow my leading
+                capabilities. </p>
+            <ul>
+                <li>Senior Software Engineer at iTech Soft Solutions (From August 2017 to August 2018)</li>
+                <li>Senior Software Engineer at Media365 Ltd. ( From May 2016 to July 2017)</li>
+                <li>Software Engineer at SoftBD Ltd. (From February 2014 to April 2016 )</li>
+                <li>Web Development Trainer at Open IT (part time) for 6 month.</li>
+            </ul>
+
+            <p>I have a working experience in <strong>Cryptocurrency, Blockchain, Cryptocurrency Trading and API</strong>. I have an experience in <strong>system administration</strong>. I can deploy project in cloud providers like as Digital Ocean, Vulter, AWS and any VPS</p>
+
+
+        </div>
+    </div>
 
 @endsection
